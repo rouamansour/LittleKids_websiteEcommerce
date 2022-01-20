3cbb62796a23fa9b232fa48b52cac924c0f03898
rouamansour <rouamansour.rm@gmail.com>
2022-01-20 19:32:23 +0000

    update

---
+++ Header.js
@@ -0,0 +1,24 @@
+document.write(`
+
+<input type="checkbox" id="check">
+	<nav>
+		<div class="icon">Little <b style="color: rgb(32, 52, 235);">Kids </b> Shop</div>
+		<div class="search_box">
+			<input type="search" placeholder="Search all iteams ...">
+			<span class="fa fa-search"></span>
+		</div>
+		<ol>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Category</a></li>
+			<li><a href="#">Cart</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#"> Login<ion-icon name="log-in-outline"></ion-icon></a></li>
+		</ol>
+		<label for="check" class="bar">
+			<span class="fa fa-bars" id="bars"></span>
+			<span class="fa fa-times" id="times"></span>
+		</label>
+	</nav>
+	
+
+`);
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Kids shop</title>
+    <link href="css/jquery.bxslider.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="css/style.css" rel="stylesheet"> -->
+    <link href="layout.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+</head>
+
+<body>
+
+    <script src="Header.js"></script>
+
+
+    <!-- slider area -->
+    <div class="slider-area">
+        <div class="slider">
+            <div>
+                <a href="#"><img src="pictures/istockphoto-1285490580-170667a.jpg"></a>
+            </div>
+            <div>
+                <a href="#"><img src="pictures/child-childrens-baby-children-s.jpg"></a>
+            </div>
+            <div>
+                <a href="#"><img src="pictures/istockphoto-941424180-612x612.jpg"></a>
+            </div>
+            <div>
+            </div>
+        </div>
+
+
+        <script src="Footer.js"></script>
+
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+        <script src="js/jquery.js"></script>
+        <!-- bxslider minified js file -->
+        <script src="js/jquery.bxslider.min.js"></script>
+        <script src="js/owl.carousel.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+</body>
+
+</html>
