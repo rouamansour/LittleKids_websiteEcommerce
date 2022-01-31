54a0f7c9280d576d814695684165466229b8f7eb
rouamansour <rouamansour.rm@gmail.com>
2022-01-31 16:48:20 +0000

    Update admin panel

---
+++ admin/includes/footer.php
@@ -0,0 +1,12 @@
+      <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+                  <span>Copyright &copy; Your Website 2022</span>
+              </div>
+          </div>
+      </footer>
+      </div>
+      </div>
+      </div>
+      </body>
+      </html>
